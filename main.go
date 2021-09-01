9ed335954fcc591ea2c0f0a099fab3c4b6054120
DesktopAndy <andy82573495@gmail.com>
2021-09-01 20:01:17 +0800

    簡單的HandleFunc

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func DefaultHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "test123")
+}
 func main() {
-	fmt.Print("test")
+	http.HandleFunc("/", DefaultHandle)
+	http.ListenAndServe(":8080", nil)
 }
