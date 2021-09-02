9f5699c66af3ca524beebd38f53aba225aff429a
DesktopAndy <andy82573495@gmail.com>
2021-09-02 17:33:21 +0800

    登入夜面簡單邏輯完成，還沒連資料庫

--- main.go
+++ main.go
@@ -2,13 +2,50 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
-func DefaultHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "test123")
+//index page
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, "Login Page")
+}
+
+//authentication page
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.Form.Get("username")
+	password := r.Form.Get("password")
+	fmt.Printf("帳號:%s\n密碼:%s\n", username, password)
+	http.Redirect(w, r, "/welcome", http.StatusSeeOther)
+}
+
+//sign up page
+func signUpHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("sign-up.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, t)
+}
+
+//welcome page
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("welcome.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, t)
 }
 func main() {
-	http.HandleFunc("/", DefaultHandle)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/auth", authHandler)
+	http.HandleFunc("/sign-up", signUpHandler)
+	http.HandleFunc("/welcome", welcomeHandler)
 	http.ListenAndServe(":8080", nil)
 }
