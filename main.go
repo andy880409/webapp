310b2523e26213a7c223a447881e3dbe78b34e4f
DesktopAndy <andy82573495@gmail.com>
2021-09-03 16:25:39 +0800

    把html放進view directory，把authentication的handler邏輯改到login，且還沒連資料庫

--- main.go
+++ main.go
@@ -3,48 +3,52 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 )
 
-//index page
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("index.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	t.Execute(w, "Login Page")
+type User struct {
+	UserName string
+	Password string
 }
 
-//authentication page
-func authHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	username := r.Form.Get("username")
-	password := r.Form.Get("password")
-	fmt.Printf("帳號:%s\n密碼:%s\n", username, password)
-	http.Redirect(w, r, "/welcome", http.StatusSeeOther)
+var tpl *template.Template
+var dbUser = map[string]User{}      //key:username | value:User Data
+var dbSession = map[string]string{} //key:session id | value:username
+
+func init() {
+	tpl = template.Must(template.ParseGlob("view/*"))
+}
+
+//index page
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		un := r.FormValue("username") //key就是html的name
+		p := r.FormValue("password")
+		fmt.Printf("帳號:%s\n密碼:%s\n", un, p)
+		http.Redirect(w, r, "/welcome", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "login.html", "Login Page")
 }
 
 //sign up page
 func signUpHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("sign-up.html")
-	if err != nil {
-		log.Fatal(err)
+	if r.Method == http.MethodPost {
+		un := r.FormValue("username")
+		p := r.FormValue("password")
+		dbUser[un] = User{UserName: un, Password: p}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
-	t.Execute(w, t)
+	tpl.ExecuteTemplate(w, "sign-up.html", nil)
 }
 
 //welcome page
 func welcomeHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("welcome.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	t.Execute(w, t)
+	tpl.ExecuteTemplate(w, "welcome.html", nil)
 }
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/auth", authHandler)
+	http.HandleFunc("/", loginHandler)
 	http.HandleFunc("/sign-up", signUpHandler)
 	http.HandleFunc("/welcome", welcomeHandler)
 	http.ListenAndServe(":8080", nil)
