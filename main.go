e83a79480101da34724e9a9932785af7440851a6
DesktopAndy <andy82573495@gmail.com>
2021-09-08 16:10:18 +0800

    簡單的帳號驗證完成，還沒設Cookie及資料庫

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -14,34 +15,64 @@ type User struct {
 var tpl *template.Template
 var dbUser = map[string]User{}      //key:username | value:User Data
 var dbSession = map[string]string{} //key:session id | value:username
-var a = 1
 
 func init() {
 	tpl = template.Must(template.ParseGlob("view/*"))
 }
 
+//檢查用戶是否存在
+func checkUserIsExist(u string) bool {
+	_, isExist := dbUser[u]
+	return isExist
+}
+
+//檢查密碼是否一致
+func checkPassword(p1 string, p2 string) error {
+	if p1 == p2 {
+		return nil
+	}
+	return errors.New("密碼不對")
+}
+
+//驗證身分
+func auth(username string, password string) error {
+	if checkUserIsExist(username) {
+		return checkPassword(password, dbUser[username].Password)
+	}
+	return errors.New("此用戶不存在")
+}
+
 //index page
 func loginHandler(w http.ResponseWriter, r *http.Request) {
+	var data string
 	if r.Method == http.MethodPost {
 		un := r.FormValue("username") //key就是html的name
 		p := r.FormValue("password")
 		fmt.Printf("帳號:%s\n密碼:%s\n", un, p)
-		http.Redirect(w, r, "/welcome", http.StatusSeeOther)
-		return
+		err := auth(un, p)
+		if err == nil {
+			http.Redirect(w, r, "/welcome", http.StatusSeeOther)
+			return
+		}
+		data = err.Error()
 	}
-	tpl.ExecuteTemplate(w, "login.html", "Login Page")
+	tpl.ExecuteTemplate(w, "login.html", data)
 }
 
 //sign up page
 func signUpHandler(w http.ResponseWriter, r *http.Request) {
+	var data string
 	if r.Method == http.MethodPost {
 		un := r.FormValue("username")
 		p := r.FormValue("password")
-		dbUser[un] = User{UserName: un, Password: p}
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
+		if _, ok := dbUser[un]; !ok {
+			dbUser[un] = User{UserName: un, Password: p}
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+		data = fmt.Sprintf("帳號:%s 已經存在了!", un)
 	}
-	tpl.ExecuteTemplate(w, "sign-up.html", nil)
+	tpl.ExecuteTemplate(w, "sign-up.html", data)
 }
 
 //welcome page
